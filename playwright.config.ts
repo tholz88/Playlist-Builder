5ae5c2549ce1ac073c0516944277b67828984ef9
tholz88 <tobias.holzer@edu.vs.ch>
2025-10-27 15:05:40 +0100

    Add functional-test with playwright

---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30 * 1000,
+  retries: 0,
+  use: {
+    baseURL: 'http://localhost:5173', // Frontend (Vite)
+    trace: 'on-first-retry',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    port: 5173,
+    reuseExistingServer: true,
+  },
+});
